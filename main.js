126aa05c6aa19f27b31384a94aff226786f0f21e
Denis-Matyukha <denmatuhawork@gmail.com>
2021-04-13 07:41:02 +0300

    medium state - task-1 is done

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ let player1 = {
     attack: function () {
         return console.log(`${this.name} Fight...`);
     },
+    changeHP: changeHP,
+    elHP: elHP,
+    renderHP: renderHP,
 };
 
 let player2 = {
@@ -21,6 +24,9 @@ let player2 = {
     attack: function () {
         return console.log(`${this.name} Fight...`);
     },
+    changeHP: changeHP,
+    elHP: elHP,
+    renderHP: renderHP,
 };
 
 function createElement(tag, className) {
@@ -60,22 +66,65 @@ function randomNumber(min, max) {
     return number;
 };
 
-function changeHP(player) {
-    const $playerLife = document.querySelector(`.player${player.player} .life`);
-    player.hp -= randomNumber(1, 20);
+// 
+// Task #1
+// Создай три функции: changeHP, elHP и renderHP
 
-    if (player.hp <= 0) {
-        player.hp = 0;
+// 1. Функция changeHP должна в аргументах принимать, на какое кол-во надо изменять HP. 
+// И решать, нужно ли отнимать или ставить 0. Больше ничего эта функция не должна делать.
+// 2. Вторая функци elHP (это именно функция) должна возвращать document.querySelector, который должен ссылаться на внутреннее поле player, которое выводит 1 или 2. 
+// 3. Третья функци renderHP должна только рендерить hp, т.е. рисовать hp при помощи style.width.
 
-        // if (player.player === 1) {
-        // $arenas.appendChild(playerWin(player2.name));
-        // } else {
-        // $arenas.appendChild(playerWin(player1.name));
-        // }
+// Все эти функции должны быть созданы один раз и использоваться одноименных методах у обоих объектах.
+// Т.е. ссылайся в них через this на свойства и методы того объекта, в котором они используются.
+// 
+
+
+// 
+// ## Task #2 (*)
+// Давайте дальше тренироваться в создании дополнительных элементов.
+// Создайте функцию `createReloadButton` внутри это функции создайте div с классом *reloadWrap* и кнопку с классом *button*
+// Текст внутри кнопки напишите Restart.
+// не забудьте вложить эту кнопку в div.
+// Стили для этих элементов уже добавил.
+
+// Для начала повесть на кнопку событие click и в функции обратного вызова вызывай метод `window.location.reload()`
+// Вторым этапом, кнопка должна появляться только в том случае когда игра закончилась.
+
+//  Ну а на этом все.
+// До встречи очень скоро...
+// 
+
+function changeHP (min = 0, max = 0) {
+    // const $playerLife = document.querySelector(`.player${player.player} .life`);
+    this.hp -= randomNumber(min, max);
+
+    if (this.hp <= 0) {
+        this.hp = 0;
     }
-    $playerLife.style.width = player.hp + '%';
-    // $playerLife.style.width = '0%';
+    console.log(this.hp);
+    // $playerLife.style.width = player.hp + '%';
 };
+
+function elHP () {
+    return document.querySelector(`.player${this.player} .life`);
+};
+
+function renderHP () {
+    elHP.call(this).style.width = this.hp + '%';
+    // elHP.call(this).style.width = this.hp - 20 + '%';
+};
+
+// function changeHP(player) {
+//     const $playerLife = document.querySelector(`.player${player.player} .life`);
+//     player.hp -= randomNumber(1, 20);
+
+//     if (player.hp <= 0) {
+//         player.hp = 0;
+//     }
+
+//     $playerLife.style.width = player.hp + '%';
+// };
 
 function playerWin(name) {
     const $winTitle = createElement('div', 'loseTitle');
@@ -86,14 +135,14 @@ function playerWin(name) {
         $winTitle.innerText = `draw`;
     }
 
-    // $randomButton.disabled = true;
-
     return $winTitle;
 }
 
 $randomButton.addEventListener('click', function () {
-    changeHP(player1);
-    changeHP(player2);
+    // !!!
+    // changeHP(player1);
+    // !!!
+    // changeHP(player2);
 
     if (player1.hp === 0 || player2.hp === 0) {
         $randomButton.disabled = true;
