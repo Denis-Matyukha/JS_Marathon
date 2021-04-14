5299774af2a46d4f4909b34779af09dc677410ee
Denis-Matyukha <denmatuhawork@gmail.com>
2021-04-15 02:24:15 +0300

    pre final state with lots of comments

--- main.js
+++ main.js
@@ -82,14 +82,14 @@ function createReloadButton() {
     $reloadBtn.innerText = 'Restart';
     $reloadWrap.appendChild($reloadBtn);
 
-    $reloadBtn.addEventListener('click', function(){
+    $reloadBtn.addEventListener('click', function () {
         window.location.reload();
     })
 
     return $reloadWrap;
 };
 
-function changeHP (min = 0, max = 0) {
+function changeHP(min = 0, max = 0) {
     this.hp -= randomNumber(min, max);
 
     if (this.hp <= 0) {
@@ -97,11 +97,11 @@ function changeHP (min = 0, max = 0) {
     }
 };
 
-function elHP () {
+function elHP() {
     return document.querySelector(`.player${this.player} .life`);
 };
 
-function renderHP () {
+function renderHP() {
     elHP.call(this).style.width = this.hp + '%';
 };
 
@@ -144,32 +144,64 @@ $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
 
 function enemyAttack() {
-    const hit = ATTACK[randomNumber(0,2)];
-    const defence = ATTACK[randomNumber(0,2)];
+    const hit = ATTACK[randomNumber(0, 2)];
+    const defence = ATTACK[randomNumber(0, 2)];
 
     return {
+        id: 2,
         value: randomNumber(0, HIT[hit]),
         hit,
         defence,
     }
 };
 
-$formFight.addEventListener('submit', function(e) {
+const getDamage = function (action, counterAction) {
+    if (action !== counterAction) {
+        console.log(`WOW! - player${this.id} caused damage to ${this.value}`);
+
+        let victim = this.id === 1 ? player2 : player1;
+
+        victim.changeHP(this.value, this.value);
+        victim.renderHP();
+
+    }
+};
+
+const checkWinners = function () {
+    if (player1.hp === 0 || player2.hp === 0) {
+        for (let item of $formFight) {
+            item.disabled = 'true';
+        };
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+        $arenas.appendChild(playerWin(player2.name));
+    } else if (player2.hp === 0 && player2.hp < player1.hp) {
+        $arenas.appendChild(playerWin(player1.name));
+    } else if (player2.hp === 0 && player1.hp === 0) {
+        $arenas.appendChild(playerWin());
+    }
+
+};
+
+$formFight.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('___001 - formFight');
-    console.log($formFight);
+    // console.log('___001 - formFight');
+    // console.log($formFight);
     console.log('___002 - formFight');
     console.dir($formFight);
 
     const enemy = enemyAttack();
-    console.log('___003 - enemy');
-    console.log(enemy);
+    // console.log('___003 - enemy');
+    // console.log(enemy);
 
-    const attack = {};
+    const attack = {
+        id: 1,
+    };
 
     for (let item of $formFight) {
-        console.log('___004 - item');
-        console.dir(item);
+        // console.log('___004 - item');
+        // console.dir(item);
 
         if (item.checked && item.name === 'hit') {
             attack.value = randomNumber(0, HIT[item.value]);
@@ -185,7 +217,57 @@ $formFight.addEventListener('submit', function(e) {
 
     console.log('___005 - attack');
     console.log(attack);
-
+    // {value: 16, hit: "body", defence: "body"}
     console.log('___006 - enemy');
     console.log(enemy);
+    // {value: 10, hit: "foot", defence: "head"}
+
+    // !!!
+    // !!!!!!
+    // check result and render hp
+
+    // [v] делаем одну проверяющую функцию
+    // [v] проверяем если не совпали удар и защита игрока - рендерим урон на противнике
+
+    // if (attack.hit !== enemy.defence) {}
+    // if (enemy.hit !== attack.defence) {}
+
+    // [v] вынести функцию за пределы обработчика
+
+    getDamage.call(attack, attack.hit, enemy.defence);
+    getDamage.call(enemy, enemy.hit, attack.defence);
+
+    checkWinners();
+
+    // [] проверяем если не совпали удар и защита компа - рендерим урон на игроке
+
+    // [] проверяем уровень жизней и выносим вердикт и рендерим кнопку рестарт и дисейблим либо всю форму, либо button
+
+    //     player1.changeHP(0,20);
+    //     player1.renderHP();
+
+    //     player2.changeHP(0,20);
+    //     player2.renderHP();
+
+    //     if (player1.hp === 0 || player2.hp === 0) {
+    //         $randomButton.disabled = true;
+    //     }
+
+    //     if (player1.hp === 0 && player1.hp < player2.hp) {
+    //         $arenas.appendChild(playerWin(player2.name));
+    //     } else if (player2.hp === 0 && player2.hp < player1.hp) {
+    //         $arenas.appendChild(playerWin(player1.name));
+    //     } else if (player2.hp === 0 && player1.hp === 0) {
+    //         $arenas.appendChild(playerWin());
+    //     }
 });
+
+/**
+ Кроме того что повторишь код в уроке, тебе следует дописать **submit**
+
+Перенеси логику из бывшей кнопки *Random* в наш новый обработчик событий, проверь, кто из игроков не попал в защиту, и сделай соответсвующие действия.
+
+Не забывай про финальный вывод сообщения, кто победил, и конечно же, кнопки **Restart**.
+
+Подумай, какую часть кода можно вынести в какие-либо дополнительные функции, чтобы не повторяться.
+ */
